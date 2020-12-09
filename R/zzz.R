2b71a9ce65ef6ae033eee52cfe1daa042441e456
Kevin Murray <foss@kdmurray.id.au>
2020-12-09 17:41:59 +1100

    Add generated files

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+#' windowlickr
+#'
+#' Windowed analyses of BCF files.
+#'
+#' @name windowlickr
+#' @docType package
+#' @author Kevin Murray
+#' @import Rcpp foreach dplyr purrr
+#' @useDynLib windowlickr
+NULL
